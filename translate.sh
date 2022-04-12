a19f9482b66d73794a425e78007e89c6b7de1bac
Inaban <Rairlord@gmail.com>
2022-04-12 18:28:05 +0300

    Leaving linux

---
+++ translate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -sf ./awesome ~/.config/awesome
+ln -sf ./alacritty ~/.config/alacritty
+ln -sf ./fish ~/.config/fish
+ln -sf ./picom ~/.config/picom
+ln -sf ./rofi ~/.config/rofi
+ln -sf ./starship.toml ~/.config/starship.toml
+
